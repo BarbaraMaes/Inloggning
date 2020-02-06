2319db0246dc39644bdc6bfa9c7fb017b4c19d8f
BarbaraMaes <maesbarbara92@gmail.com>
2020-02-06 22:03:59 +0100

    x

--- classes/Database.php
+++ classes/Database.php
@@ -7,26 +7,60 @@ class Database {
     private $password = "1234";
     private $dbname = "inloggning";
 
-    private $dbh; 
+    private $pdo; 
     private $error; 
     private $stmt; 
 
     public function __construct(){
         //Set DSN 
-        $dsn = "mysql:host=$this->host;dbname=$this->dbname";
+        $dsn = 'mysql:host=' . $this->host. ';dbname='. $this->dbname;
         //Set Options 
         $options = array(
-            PDO:ATTR_PERSISTENT => true, 
-            PDO:ATTR_ERRMOTDE => PDO::ERRMODE_EXCEPTION
+            PDO::ATTR_PERSISTENT => true, 
+            PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
         );
         // new PDO instance 
         try{
-
-        }catch(){
-            
+            $this->pdo = new PDO($dsn, $this->user, $this->password, $options);
+        }catch(PDOException $e){
+            $this->error = $e->getMessage();
         }
     }
 
+    public function query($sql){
+        $this->stmt = $this->pdo->prepare($sql);
+    }
+
+    public function bind($param, $value, $type = null){
+        if(is_null($type)){
+            switch(true){
+                case is_int($value): 
+                    $type = PDO::PARAM_INT;
+                break; 
+
+                case is_bool($value):
+                    $type = PDO::PARAM_BOOL;
+                break; 
+
+                case is_null($value): 
+                    $type = PDO::PARAM_NULL;
+                break; 
+
+                default: 
+                    $type = PDO::PARAM_STR;
+            }
+        }
+        $this->stmt->bindValue($param, $value, $type);
+    }
+
+    public function execute(){
+        return $this->stmt->execute();
+    }
+
+    public function resultset(){
+        $this->execute();
+        return $this->stmt->fetchAll(PDO::FETCH_ASSOC);
+    }
 }
 
 ?>
--- index.php
+++ index.php
@@ -1,5 +1,11 @@
 <?php
+    require "classes/Database.php";
 
+    $db = new Database();
 
+    $db->query("SELECT * FROM users WHERE id = :id");
+    $db->bind(":id", 1);
+    $rows = $db->resultset();
+    print_r($rows);
 
 ?>
